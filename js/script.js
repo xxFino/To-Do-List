b883851ce403241b17f441d0e26fbedb7f5a19bb
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2022-12-29 20:07:19 +0100

    added autofocus in input and blocked the abillity to add a white signs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 {
     const tasks = [
-        
+
     ];
 
     const addNewTask = (newTaskContent) => {
@@ -62,13 +62,15 @@
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        const newTaskElement = document.querySelector(".js-newTask")
+        const newTaskContent = newTaskElement.value.trim();
 
-        if (newTaskContent === "") {
-            return;
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskElement.value = ""            
         };
 
-        addNewTask(newTaskContent);
+        newTaskElement.focus();
     };
 
     const init = () => {
