241650baced8534bf5818097b543217751414905
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-01-03 17:43:04 +0100

    corrected removeTask refactor with compatibility to immutability

--- js/script.js
+++ js/script.js
@@ -11,8 +11,9 @@
     };
 
     const removeTask = (taskIndex) => {
-        [
-            ...tasks.splice(taskIndex, 1),
+       tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
         ];
         render();
     };
