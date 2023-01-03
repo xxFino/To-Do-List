f02c847944f09495d37663efcfc568157fc4ebdd
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-01-04 00:33:35 +0100

    toogleTaskDone refactor with compatibility to immutability

--- js/script.js
+++ js/script.js
@@ -18,8 +18,16 @@
         render();
     };
 
-    const toogleTaskDone = (taskIndex) => {        
-        tasks[taskIndex].done = !tasks[taskIndex].done
+    const toogleTaskDone = (taskIndex) => { 
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            {
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done,
+            },
+            ...tasks.slice(taskIndex + 1),
+        ];
+
         render();
     };
 
